e116b6f5f33b36ed6e52ebd38022a6d91599bc5f
Shreya Srivastava <112487189+shreyasrivastava11@users.noreply.github.com>
2026-04-18 20:59:02 +0530

    Refactor houseRobber.java to eliminate duplicates
    Removed duplicate Solution class and method definitions.

--- Recursion/houseRobber.java
+++ Recursion/houseRobber.java
@@ -2,22 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         return adjRob(nums, 0);
     }
-class Solution {
-    public int rob(int[] nums) {
-        return adjRob(nums, 0);
-    }
 
-    public int adjRob(int[] nums, int i){
-        if(i >= nums.length){
-            return 0;
-        }
-
-        int take = nums[i] + adjRob(nums, i+2);
-        int notTake = adjRob(nums, i+1);
-
-        return Math.max(take, notTake);
-    }
-}
     public int adjRob(int[] nums, int i){
         if(i >= nums.length){
             return 0;
